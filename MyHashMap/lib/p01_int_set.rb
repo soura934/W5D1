7996c261b2a68cb1ac1cc84c87b9909a91787bbc
Yehudah <jros987@gmail.com>
2020-11-23 14:09:20 -0500

    int_set finished

--- MyHashMap/lib/p01_int_set.rb
+++ MyHashMap/lib/p01_int_set.rb
@@ -42,20 +42,29 @@ class IntSet
   end
 
   def insert(num)
+    if !include?(num)
+      self[num % num_buckets].push(num)
+    end
   end
 
   def remove(num)
+    
+    self[num].delete(num) if include?(num)
+     
   end
 
   def include?(num)
+     
+    self[num].include?(num) 
   end
 
+  
   private
-
   def [](num)
     # optional but useful; return the bucket corresponding to `num`
+   @store[num % num_buckets]
   end
-
+ 
   def num_buckets
     @store.length
   end
