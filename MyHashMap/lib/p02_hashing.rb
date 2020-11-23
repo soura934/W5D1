a03f35b9d6139944a9ffc52550fdf8737cec66fe
Yehudah <jros987@gmail.com>
2020-11-23 16:36:31 -0500

    created some of hash_set'

--- MyHashMap/lib/p02_hashing.rb
+++ MyHashMap/lib/p02_hashing.rb
@@ -1,10 +1,19 @@
 class Integer
   # Integer#hash already implemented for you
 end
-
+# same two array to produce same result, diff arrays diff results. Order matters.
+# also efficency, not worrying about that here.
 class Array
+  
   def hash
+     result = 0
+    self.each_with_index do |el, idx|
+      result += (idx ^ idx + 1)  
+      result = result.hash + el.hash   
+    end
+    result
   end
+  
 end
 
 class String
