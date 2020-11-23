31d0bb6f657692ec15f4b129a5d9f82fa2b2940a
soura934 <soura934@gmail.com>
2020-11-23 17:07:02 -0500

    doing hash set

--- MyHashMap/lib/p03_hash_set.rb
+++ MyHashMap/lib/p03_hash_set.rb
@@ -6,19 +6,33 @@ class HashSet
     @count = 0
   end
 
-  def insert(key)
+  def insert(num)
+    if !include?(num)
+      self[num] << num
+      @count += 1
+    end
+    if num_buckets < count
+      resize!
+    end
   end
 
-  def include?(key)
+  def include?(num)
+    self[num].include?(num) 
   end
 
-  def remove(key)
+  def remove(num)
+    if include?(num)
+      self[num].delete(num) 
+      @count -= 1
+     end
   end
 
   private
 
   def [](num)
     # optional but useful; return the bucket corresponding to `num`
+    new_hash = num.hash
+    @store[new_hash % num_buckets]
   end
 
   def num_buckets
@@ -26,5 +40,14 @@ class HashSet
   end
 
   def resize!
+    num_buckets = @store.length * 2
+    old_backet = @store
+    @store = Array.new(num_buckets){Array.new}
+    @count = 0
+    old_backet.each do |bucket|
+      bucket.each do |num|
+        insert(num)
+      end
+    end
   end
 end
